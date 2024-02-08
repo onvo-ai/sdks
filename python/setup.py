d10ac5aecfec43c423a8d60ef63fcaf7fc951f0f
Bryan Davis <bryandavis999@gmail.com>
2024-02-09 01:00:14 +0530

    Created a basic python package

---
+++ python/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="onvo",
+    version="0.0.1",
+    author="Bryan Davis",
+    author_email="bryandavis999.dev@gmail.com",
+    description="Communicate with the Onvo platform.",
+    long_description="A gem to provide utilities to seamlessly communicate with the Onvo platform, allowing developers to integrate AI-powered dashboards into their products.",
+    packages=find_packages(),
+    install_requires=[],
+    keywords=["python", "onvo"],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+)
