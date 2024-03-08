d4e1136f11764aa1473bc516d249a23d7e792065
Ronnel Davis <ronneldavis1996@gmail.com>
2024-03-08 15:13:06 +0530

    Updates

--- js/lib/base.d.ts
+++ js/lib/base.d.ts
@@ -1,9 +1,8 @@
-import { Method } from "axios";
 export default class OnvoBase {
     #private;
     endpoint: string;
-    fetchBase(url: string, method?: Method, body?: any): Promise<any>;
-    streamingFetch(url: string, method: Method, body: any, callbacks: {
+    fetchBase(url: string, method?: "GET" | "PUT" | "POST" | "DELETE" | "PATCH" | "OPTIONS", body?: any): Promise<any>;
+    streamingFetch(url: string, method: "GET" | "PUT" | "POST" | "DELETE" | "PATCH" | "OPTIONS", body: any, callbacks: {
         onStream: (str: string) => void;
         onComplete: (str: string) => void;
         onError: (err: Error) => void;
