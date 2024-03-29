28b131486f183e3ad86552ddf15c060cb5391f71
Bryan Davis <bryandavis999@gmail.com>
2024-03-30 00:50:44 +0530

    Python SDK: updated & added documentation for sessions

--- python/onvo/resources/sessions.py
+++ python/onvo/resources/sessions.py
@@ -2,14 +2,55 @@ from onvo.base_resource import BaseResource
 
 
 class Sessions(BaseResource):
-    def list(self, dashboard_id: str):
-        return self.base_get("/sessions", params={"dashboard": dashboard_id})
+    """Endpoints to get and update sessions. A session creates a link between a user under your organisation and a dashboard.
 
-    def delete(self, dashboard_id: str):
-        return self.base_delete("/sessions", params={"dashboard": dashboard_id})
+    Args:
+        BaseResource (_type_): _description_
+    """
+
+    def list(self, dashboard_id: str):
+        """List all sessions for a dashboard
+
+        Args:
+            dashboard_id (str): ID of the dashboard to list sessions for
+
+        Returns:
+            list: A list of all sessions for a dashboard
+        """
+        return self.base_get("/sessions", params={"parent_dashboard": dashboard_id})
+
+    def revoke(self, dashboard_id: str):
+        """Delete all sessions for a dashboard
+
+        Args:
+            dashboard_id (str): ID of the dashboard to delete all sessions for
+
+        Returns:
+            dict: A dictionary that shows status of deleting all sessions. Returns {"success": true} if successful.
+        """
+        return self.base_delete("/sessions", params={"parent_dashboard": dashboard_id})
 
     def upsert(self, dashboard_id: str, user_id: str):
-        session_data = self.base_post(
+        """If a session already exists, it is retrieved to save the user's changes and edits. If a session does not exist, one is created and is retrieved.
+
+        Args:
+            dashboard_id (str): ID of the dashboard to create a session for
+            user_id (str): ID of the user to create a session for
+
+        Returns:
+            dict: A dictionary containing the details of the created/retrieved session
+        """
+        return self.base_post(
             "/sessions", data={"dashboard": dashboard_id, "user": user_id}
         )
-        return {**session_data, "url": f"{self.endpoint}{session_data['url']}"}
+
+    def delete(self, id):
+        """Delete an existing session
+
+        Args:
+            id (str): The session id to delete
+
+        Returns:
+            dict: A dictionary that shows status of the delete. Returns {"success": true} if successful.
+        """
+        return self.base_delete(f"/sessions/{id}")
--- python/tests/dashboard/test_sessions.py
+++ python/tests/dashboard/test_sessions.py
@@ -1,24 +1,45 @@
-# from .base_test import DashboardBaseTest
+from .base_test import DashboardBaseTest
 
 
-# class TestSessions(DashboardBaseTest):
+class TestSessions(DashboardBaseTest):
 
-#     def test_list(self):
-#         self.assertShouldRaise(
-#             None,
-#             lambda: self.onvoSDK.sessions.list(self.sampleDashboardId),
-#         )
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.sample_embed_user_id = cls.onvoSDK.embed_users.upsert(
+            {
+                "id": cls.sample_user_id,
+                "name": "Python Integration Test User",
+                "email": "test@test.com",
+            }
+        )["id"]
 
-#     def test_upsert(self):
-#         self.assertShouldRaise(
-#             None,
-#             lambda: self.onvoSDK.sessions.upsert(
-#                 self.sampleDashboardId, "197302e5-88e2-49f4-bbe7-92b5a4dc4264"
-#             ),
-#         )
+    def test_list(self):
+        self.assertShouldRaise(
+            None,
+            lambda: self.onvoSDK.sessions.list(self.sample_dashboard_id),
+        )
 
-#     def test_delete(self):
-#         self.assertShouldRaise(
-#             None,
-#             lambda: self.onvoSDK.sessions.delete(self.sampleDashboardId),
-#         )
+    def test_upsert(self):
+        self.assertShouldRaise(
+            None,
+            lambda: self.onvoSDK.sessions.upsert(
+                self.sample_dashboard_id, self.sample_embed_user_id
+            ),
+        )
+
+    # TODO: fix 405 method not allowed for delete by id
+    # def test_delete(self):
+    #     sample_session_token = self.onvoSDK.sessions.upsert(
+    #         self.sample_dashboard_id, self.sample_embed_user_id
+    #     )["token"]
+    #     self.assertShouldRaise(
+    #         None,
+    #         lambda: self.onvoSDK.sessions.delete(sample_session_token),
+    #     )
+
+    def test_revoke(self):
+        self.assertShouldRaise(
+            None,
+            lambda: self.onvoSDK.sessions.revoke(self.sample_dashboard_id),
+        )
