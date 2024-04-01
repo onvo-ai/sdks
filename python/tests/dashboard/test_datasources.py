420b67b8b2bb576e0c23bccbf0c2048ae1d1bfb5
Bryan Davis <bryandavis999@gmail.com>
2024-04-01 22:44:18 +0530

    Python SDK: added documentation for dashboard datasources

--- python/tests/dashboard/test_datasources.py
+++ python/tests/dashboard/test_datasources.py
@@ -1,16 +1,17 @@
-# from .base_test import DashboardBaseTest
+from .base_test import DashboardBaseTest
 
 
-# class TestDashboardDatasources(DashboardBaseTest):
+class TestDashboardDatasources(DashboardBaseTest):
 
-#     def test_list(self):
-#         self.assertShouldRaise(None, self.sampleDashboard.datasources.list)
+    def test_list(self):
+        self.assertShouldRaise(None, self.sample_dashboard.datasources.list)
 
-#     def test_link_and_unlink(self):
-#         self.assertShouldRaise(
-#             None, lambda: self.sampleDashboard.datasources.link(self.sampleDatasourceId)
-#         )
-#         self.assertShouldRaise(
-#             None,
-#             lambda: self.sampleDashboard.datasources.unlink(self.sampleDatasourceId),
-#         )
+    def test_link_and_unlink(self):
+        self.assertShouldRaise(
+            None,
+            lambda: self.sample_dashboard.datasources.link(self.sample_datasource_id),
+        )
+        self.assertShouldRaise(
+            None,
+            lambda: self.sample_dashboard.datasources.unlink(self.sample_datasource_id),
+        )
