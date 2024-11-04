f1e58589433e54801e3c40f964f50799f256cf58
Afeedh Shaji <afeedhshaji@disroot.org>
2024-11-04 12:32:55 +0530

    complete tests for automations

--- rust/tests/automations_tests.rs
+++ rust/tests/automations_tests.rs
@@ -92,4 +92,100 @@ async fn test_create_automation_integration() {
 async fn test_update_automation_integration() {
     let client = helpers::setup_client();
     let automations = Automations::new(&client);
+
+    // Create a dummy automation to update later
+    let automation_payload = &Automation {
+        id: None,
+        created_at: None,
+        last_updated_at: None,
+        created_by: None,
+        last_run_at: None,
+        next_run_at: None,
+        title: String::from("API datasource test"),
+        description: Some(String::from("A sample description")),
+        dashboard: String::from("09720600-cab3-4486-bc14-10f014f573a5"),
+        output_format: String::from("pdf"),
+        schedule: String::from(""),
+        recipient_type: None,
+        email_format: None,
+        email_subject: Some(String::from("This is an automation from Onvo")),
+        recipients: None,
+        team: None,
+        enabled: false,
+        last_updated_by: None,
+        timezone: String::from("Asia/Calcutta"),
+        method: String::from("email"),
+        email_body: String::from("This is an automation from Onvo"),
+        email_to: None,
+    };
+    let result = automations.create(&automation_payload).await;
+    assert!(result.is_ok());
+
+    // Update the automation
+    let id = &result.unwrap().id.clone().unwrap();
+    let updated_automation = &Automation {
+        id: None,
+        created_at: None,
+        last_updated_at: None,
+        created_by: None,
+        last_run_at: None,
+        next_run_at: None,
+        title: String::from("API datasource test"),
+        description: Some(String::from("A sample description")),
+        dashboard: String::from("09720600-cab3-4486-bc14-10f014f573a5"),
+        output_format: String::from("pdf"),
+        schedule: String::from(""),
+        recipient_type: None,
+        email_format: None,
+        email_subject: Some(String::from("This is an automation from Onvo")),
+        recipients: None,
+        team: None,
+        enabled: false,
+        last_updated_by: None,
+        timezone: String::from("Asia/Tokyo"), // Change timezone
+        method: String::from("email"),
+        email_body: String::from("This is an automation from Onvo"),
+        email_to: None,
+    };
+    let result = automations.update(&id, &updated_automation).await;
+    assert!(result.is_ok());
+}
+
+#[tokio::test]
+async fn test_delete_automation_integration() {
+    let client = helpers::setup_client();
+    let automations = Automations::new(&client);
+
+    // Create a dummy automation to delete later
+    let result = automations
+        .create(&Automation {
+            id: None,
+            created_at: None,
+            last_updated_at: None,
+            created_by: None,
+            last_run_at: None,
+            next_run_at: None,
+            title: String::from("API datasource test"),
+            description: Some(String::from("A sample description")),
+            dashboard: String::from("09720600-cab3-4486-bc14-10f014f573a5"),
+            output_format: String::from("pdf"),
+            schedule: String::from(""),
+            recipient_type: None,
+            email_format: None,
+            email_subject: Some(String::from("This is an automation from Onvo")),
+            recipients: None,
+            team: None,
+            enabled: false,
+            last_updated_by: None,
+            timezone: String::from("Asia/Calcutta"),
+            method: String::from("email"),
+            email_body: String::from("This is an automation from Onvo"),
+            email_to: None,
+        })
+        .await;
+    assert!(result.is_ok());
+
+    let id = &result.unwrap().id.clone().unwrap();
+    let result = automations.delete(&id).await;
+    assert!(result.is_ok());
 }
