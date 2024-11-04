a822e077d78477d623cea49fedd093862616be5a
Afeedh Shaji <afeedhshaji@disroot.org>
2024-11-04 14:42:48 +0530

    comments for datasources api

--- rust/src/resources/datasources/api.rs
+++ rust/src/resources/datasources/api.rs
@@ -11,30 +11,110 @@ impl<'a> Datasources<'a> {
         Self { client }
     }
 
+    /// List all datasources
+    ///
+    /// # Arguments
+    ///
+    /// * `id` - ID of the datasource
+    ///
+    /// # Returns
+    ///
+    /// A list of datasources
+    ///
+    /// # Error
+    ///
+    /// * `ApiError` - Error returned by the API
+    ///
     pub async fn list(&self) -> Result<Vec<Datasource>, ApiError> {
         self.client.get("/datasources").await
     }
 
+    /// Get a datasource
+    ///
+    /// # Arguments
+    ///
+    /// * `id` - ID of the datasource
+    ///
+    /// # Returns
+    ///
+    /// A datasource
+    ///
+    /// # Error
+    ///
+    /// * `ApiError` - Error returned by the API
     pub async fn get(&self, id: &str) -> Result<Datasource, ApiError> {
         self.client.get(&format!("/datasources/{}", id)).await
     }
 
+    /// Create a datasource
+    ///
+    /// # Arguments
+    ///
+    /// * `body` - A datasource
+    ///
+    /// # Returns
+    ///
+    /// A datasource
+    ///
+    /// # Error
+    ///
+    /// * `ApiError` - Error returned by the API
     pub async fn create(&self, body: &Datasource) -> Result<Datasource, ApiError> {
         self.client.put("/datasources", body).await
     }
 
+    /// Initialize a datasource
+    ///
+    ///  # Arguments
+    ///
+    /// * `id` - The id of the datasource
+    ///
+    /// # Returns
+    ///
+    /// A datasource
+    ///
+    ///  # Error
+    ///
+    /// * `ApiError` - Error returned by the API
     pub async fn initialize(&self, id: &str) -> Result<Datasource, ApiError> {
         self.client
             .get(&format!("/datasources/{}/initialize", id))
             .await
     }
 
+    /// Update a datasource
+    ///
+    /// # Arguments
+    ///
+    /// * `id` - The id of the datasource
+    /// * `body` - The datasource
+    ///
+    /// # Returns
+    ///
+    /// A datasource
+    ///
+    /// # Error
+    ///
+    /// * `ApiError` - Error returned by the API
     pub async fn update(&self, id: &str, body: &Datasource) -> Result<Datasource, ApiError> {
         self.client
             .post(&format!("/datasources/{}", id), body)
             .await
     }
 
+    /// Deletes a datasource
+    ///
+    /// # Arguments
+    ///
+    /// * `id` - The id of the datasource
+    ///
+    /// # Returns
+    ///
+    /// A datasource
+    ///
+    /// # Error
+    ///
+    /// * `ApiError` - Error returned by the API
     pub async fn delete(&self, id: &str) -> Result<(), ApiError> {
         self.client.delete(&format!("/datasources/{}", id)).await
     }
